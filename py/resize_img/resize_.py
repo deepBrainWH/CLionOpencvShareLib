b4e15058230f3e330d286cfea1efe1632fc7828f
wangheng <673602165@qq.com>
2019-03-22 19:13:52 +0800

    create linux branch

--- py/resize_img/resize_.py
+++ py/resize_img/resize_.py
@@ -3,5 +3,5 @@ dir = "C:\\Users\\wangheng\\Documents\\software_cup\\train_card_images\\"
 file_path = dir + "8.jpg"
 file_path = file_path.encode("utf8")
 outpu_path = (dir + "resize-8.jpg").encode("utf8")
-lib = ctypes.cdll.LoadLibrary("../../lib/libCLionOpencv.dll")
+lib = ctypes.cdll.LoadLibrary("../../lib_winX64/libCLionOpencv.dll")
 lib.resize_image(file_path, 408, 306, outpu_path)
