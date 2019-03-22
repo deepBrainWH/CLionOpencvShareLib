6ef06add189cffed8e6b0976dae76bcffbd94fad
wangheng <673602165@qq.com>
2019-03-23 02:11:32 +0800

    test batch normalization

--- py/test/test_BN.py
+++ py/test/test_BN.py
@@ -5,16 +5,21 @@ import shutil
 if path.exists("./log"):
     shutil.rmtree("./log")
 
+input_ = np.random.randint(0, 100, (1000,100))
+output_ = np.random.randint(0, 100, (1000, 10))
+
+
 x = tf.placeholder(tf.float32, [None, 100])
+y = tf.placeholder(tf.float32, [None, 10])
 m, v = tf.nn.moments(x, [0])
 tf.summary.histogram("x", x)
 bn_x = tf.nn.batch_normalization(x, m, v, 0, 1, 0.01)
 tf.summary.histogram("bn_x", bn_x)
 
-w = tf.ones([100, 300])
-b = tf.random.normal([1, 300])
+w = tf.Variable(tf.ones([100, 300]))
+b = tf.Variable(tf.random.normal([1, 300]))
 pre = tf.matmul(bn_x, w) + b
-
+pre = tf.nn.sigmoid(pre)
 tf.summary.histogram("pre", pre)
 
 
@@ -29,19 +34,25 @@ epsilon = tf.ones([300])*0.1
 bn_pre = tf.nn.batch_normalization(pre, mean_, var_, offset, scale, epsilon, 'batch_normalization')
 
 tf.summary.histogram("bn_pre", bn_pre)
-w2 = tf.random.normal([300, 20])
-b2 = tf.random.normal([1, 20])
+w2 = tf.Variable(tf.random.normal([300, 10]))
+b2 = tf.Variable(tf.random.normal([1, 10]))
 pre2 = tf.matmul(bn_pre, w2) + b2
-
-
+pre2 = tf.nn.relu(pre2)
 tf.summary.histogram("pre2", pre2)
+
+loss = tf.losses.mean_squared_error(y, pre2)
+tf.summary.scalar("loss", loss)
+opt = tf.train.AdadeltaOptimizer(0.001).minimize(tf.reduce_sum(loss))
+
 sess = tf.InteractiveSession()
 merge_all = tf.summary.merge_all()
-input_ = np.random.randint(0, 100, (1000,100))
-input_t = tf.convert_to_tensor(input_,tf.float32)
 sess.run(tf.global_variables_initializer())
 writer = tf.summary.FileWriter("./log", sess.graph)
-_, result = sess.run([merge_all, pre2], feed_dict={x: input_})
-writer.add_summary(_)
-print(result)
+for i in range(200000):
+    result = sess.run([opt], feed_dict={x: input_,  y: output_})
+    if i % 10 == 0:
+        _, los_ = sess.run([merge_all, loss], feed_dict={x: input_,  y: output_})
+        print("step %d, loss value is %.4f" % (i, los_))
+        writer.add_summary(_, i)
+writer.close()
 sess.close()
